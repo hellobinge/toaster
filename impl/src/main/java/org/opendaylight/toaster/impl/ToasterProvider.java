6cd54d58a54144c81b60b5ac7906246955517a04
Libin Song <libinsong2020@u.northwestern.edu>
2016-04-09 01:20:52 -0500

    add onDataChanged

--- impl/src/main/java/org/opendaylight/toaster/impl/ToasterProvider.java
+++ impl/src/main/java/org/opendaylight/toaster/impl/ToasterProvider.java
@@ -7,23 +7,54 @@
  */
 package org.opendaylight.toaster.impl;
 
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.toaster.rev150105.Toaster;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ToasterProvider implements BindingAwareProvider, AutoCloseable {
+public class ToasterProvider implements BindingAwareProvider, DataChangeListener, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(ToasterProvider.class);
 
+    private ProviderContext providerContext;
+    private DataBroker dataService;
+    private ListenerRegistration<DataChangeListener> dcReg;
+
+    public static final InstanceIdentifier<Toaster> TOASTER_ID = InstanceIdentifier.builder(Toaster.class).build();
+
     @Override
     public void onSessionInitiated(ProviderContext session) {
+        providerContext = session;
+        dataService = session.getSALService(DataBroker.class);
+        dcReg = dataService.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, TOASTER_ID, this, DataChangeScope.SUBTREE);
         LOG.info("ToasterProvider Session Initiated");
     }
 
     @Override
     public void close() throws Exception {
+        if (dcReg != null) {
+            dcReg.close();
+        }
         LOG.info("ToasterProvider Closed");
     }
 
+    @Override
+    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> asyncDataChangeEvent) {
+        DataObject d = asyncDataChangeEvent.getUpdatedSubtree();
+        if (d instanceof Toaster) {
+            Toaster t = (Toaster) d;
+            LOG.info("onDataChange - new Toaster config: ", t);
+        } else {
+            LOG.info("onDataChange - not Toaster config: ", d);
+        }
+    }
 }
